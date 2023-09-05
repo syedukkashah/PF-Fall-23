f6df145debb3fd21daf796d0661ef0f978ccf6c0
ukkashah11 <115150510+ukkashah11@users.noreply.github.com>
2023-09-05 10:22:17 +0500

    Update saleprice.c

--- Lab/03/Ex3/saleprice.c
+++ Lab/03/Ex3/saleprice.c
@@ -1,1 +1,8 @@
-
+#include <stdio.h>
+#include <math.h>
+int main(){
+          int originalprice = 1500; //declaration + initialization
+          int saleprice = 0.8*1500;
+          printf("The original price is %d", originalprice);
+          printf("\nThe sale price is %d", saleprice);
+}//end main
