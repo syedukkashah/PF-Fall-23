1a9a34b7557f302f734b3039731bb671bc87d706
ukkashah11 <115150510+ukkashah11@users.noreply.github.com>
2023-09-05 10:08:34 +0500

    Update volume.c

--- Lab/03/Ex1/volume.c
+++ Lab/03/Ex1/volume.c
@@ -1,1 +1,13 @@
 
+#include <stdio.h>
+int main(){
+          int height, width, length,volume; //declaration 
+          height = 20; // initializations
+          width = 10;
+          length = 10;
+          volume = height*width*length;
+          printf("Width of the container is %d cm", width);
+          printf("\nLength of the container is %d cm", length);
+          printf("\nHeight of the container is %d cm", height);
+          printf("\nVolume of the container is %d cubic cm", volume);
+}//end main
