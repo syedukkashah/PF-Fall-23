6113cd3d697fd4a0ce48dd207128ba81fcf0aebb
ukkashah11 <115150510+ukkashah11@users.noreply.github.com>
2023-09-05 10:17:50 +0500

    Update hypotenuse.c

--- Lab/03/Ex2/hypotenuse.c
+++ Lab/03/Ex2/hypotenuse.c
@@ -1,1 +1,11 @@
-
+#include <stdio.h>
+#include <math.h>
+int main(){
+          int opposite, adjacent, hypotenuse; //declaration 
+          printf("Enter length of adjacent side: ");
+          scanf("%d", &adjacent);
+          printf("Enter length of opposite side: ");
+          scanf("%d", &opposite);
+          hypotenuse = sqrt(pow(adjacent, 2) + pow(opposite, 2));
+          printf("Hypotenuse is %d units", hypotenuse);
+}//end main
