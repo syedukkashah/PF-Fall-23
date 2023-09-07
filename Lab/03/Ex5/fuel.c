874e30fe3d7935502a3443feb4b39d65261d7822
ukkashah11 <115150510+ukkashah11@users.noreply.github.com>
2023-09-07 22:19:07 +0500

    Create fuel.c

---
+++ Lab/03/Ex5/fuel.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main() {
+    int totaldist = 40;
+    int fuel = 2*totaldist;
+    printf("The total distance travelled was %d and %d litres of fuel was consumed in the journey", totaldist, fuel);
+}
