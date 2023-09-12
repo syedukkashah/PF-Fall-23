c073db829a000c1af1f6b993463c1559f3119b1d
ukkashah11 <115150510+ukkashah11@users.noreply.github.com>
2023-09-12 10:36:43 +0500

    Create Q2.c

---
+++ Lab/04/Q2.c
@@ -0,0 +1,18 @@
+
+#include <stdio.h>
+int main(){
+        int n, i, j;
+        printf("Enter number of rows: ");
+        scanf("%d", &n);
+        i=0; 
+        while(i<n){
+               i++;
+               j=0;
+               while(j<i){ 
+                    printf("*");
+                     j++;
+                }
+                printf("\n");
+        }
+        return 1;
+}
