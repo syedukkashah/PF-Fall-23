90381fd7b11bbe346df0614e446a23fc1fc7ecc1
ukkashah11 <115150510+ukkashah11@users.noreply.github.com>
2023-09-12 10:42:49 +0500

    Create Q6.c

---
+++ Lab/04/Q6.c
@@ -0,0 +1,60 @@
+
+#include <stdio.h>
+
+int main()
+{
+
+	int n, rows = 1, columns;
+        printf("Enter number of rows: ");
+        scanf("%d", &n);
+        if(n%2 == 0){
+            n=n-1;
+          }
+	while (rows <= n) {
+		columns = n;
+
+		while (columns > rows) {
+			printf(" ");
+			columns--;
+		}
+
+		printf("*");
+		columns = 1;
+		while (columns < (rows - 1) * 2) {
+			printf(" ");
+			columns++;
+		}
+		if (rows == 1) {
+			printf("\n");
+		}
+		else {
+			printf("*\n");
+		}
+		rows++;
+	}
+
+	rows = n - 1;
+	while (rows >= 1) {
+		columns = n;
+		
+		while (columns > rows) {
+			printf(" ");
+			columns--;
+		}
+	
+		printf("*");
+		columns = 1;
+		while (columns < (rows - 1) * 2) {
+			printf(" ");
+			columns++;
+		}
+		if (rows == 1) {
+			printf("\n");
+		}
+		else {
+			printf("*\n");
+		}
+		rows--;
+	}
+	return 0;
+}
