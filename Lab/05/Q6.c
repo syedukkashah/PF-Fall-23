8b71d762079dacaa0c5a24687371cd9b8698b7ca
ukkashah11 <115150510+ukkashah11@users.noreply.github.com>
2023-09-19 21:38:20 +0500

    Update Q6.c

--- Lab/05/Q6.c
+++ Lab/05/Q6.c
@@ -1,18 +1,14 @@
 #include <stdio.h>
-int main(){
-	int num;
-	printf("Enter 8-bit number: ");
-	scanf("%d", &num);
-	if((num/10000)%10 == 1) && (num/1000000)%10 == 1)){
-		num-= 1001000;
+int main() {
+    int num; 
+    printf("Enter 8 bit binary number: ");
+    scanf("%d", &num);
+    if((num/1000)%10 == 1 && (num/1000000)%10 ==1 ){
+    	printf("bit 4 and bit 7 are ON\n");
+    	num = num - 1001000;
+    	printf("%d",num);
 	}
-	if((num/10000)%10 == 1){
-		num-=10000;
-		printf("%d", num);
+	else{
+		printf("bit 4 and bit 7 are OFF");
 	}
-	if((num/1000000)%10 == 1){
-		num-=1000000;
-		printf("%d", num);
-	}
-	
 }
