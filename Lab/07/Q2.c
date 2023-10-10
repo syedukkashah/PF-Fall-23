d4252d3fb29cee42ba02692d48f75ce42682f2e4
ukkashah11 <115150510+ukkashah11@users.noreply.github.com>
2023-10-10 09:14:39 +0500

    Create Q2.c

---
+++ Lab/07/Q2.c
@@ -0,0 +1,30 @@
+/*
+Programmer: Syed Ukkashah
+Date: 12th Sept 2023
+Description: Shifting Array elements
+*/
+#include <stdio.h>
+int main()
+{
+    int arr[7] = {1, 2, 3, 4, 5, 6, 7};
+    int d = 2;
+    printf("Array Elements: \n");
+    for (int i = 0; i < 7; i++)
+    {
+        printf("%d, ", arr[i]);
+    }
+    for (int i = 0; i < d; i++)
+    {
+        int temp = arr[0];
+        for (int j = 0; j < 7; j++)
+        {
+            arr[j] = arr[j + 1];
+        }
+        arr[6] = temp;
+    }
+    printf("\nArray Elements: \n");
+    for (int i = 0; i < 7; i++)
+    {
+        printf("%d, ", arr[i]);
+    }
+}
