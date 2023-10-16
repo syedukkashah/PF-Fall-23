abd6a70744ddc5a3a0726a2a18d1717b85ad6efd
ukkashah11 <115150510+ukkashah11@users.noreply.github.com>
2023-10-16 21:51:11 +0500

    Create barcodevalidation.c

---
+++ practice/barcodevalidation.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdbool.h>
+int main(){
+	int x, odd=0, even=0,total;
+	long code, arr[12];
+printf("Enter a 12 digit bar code: ");
+scanf("%l", &code);
+for(int i=0; i<12; i++){
+	x=code%10;
+	arr[i]=x;
+	code/=10;
+}
+for(int i=0; i<12; i++){
+	if((i+1)%2==0){
+		even+=arr[i];
+	}
+	else{
+		odd+=arr[i];
+	}
+}
+total = (odd*2)+even;
+if(total%10 == 0 && arr[11]==0){
+	printf("Check Digit = Last Digit\n");
+	printf("This Barcode is Valid");
+}
+else{
+	printf("This Barcode is Invalid");
+}
+}
