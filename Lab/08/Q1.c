80aa14c3ddededbc36e5e247c733f02eb2d61447
ukkashah11 <115150510+ukkashah11@users.noreply.github.com>
2023-10-24 08:24:01 +0500

    Update Q1.c

--- Lab/08/Q1.c
+++ Lab/08/Q1.c
@@ -1,1 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+void swapInteger(int a, int b){
+	int temp = a;
+	a=b;
+	b=temp;
+	printf("Value of a is %d\n", a);
+	printf("Value of b is %d\n", b);
+}
+
+int main() {
+	int x,y;
+	printf("Enter a number a: ");
+	scanf("%d", &x);
+	printf("Enter a number b: ");
+	scanf("%d", &y);
+	swapInteger(x,y);
+	return 0;
+}
