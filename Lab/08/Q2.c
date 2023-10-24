11c345b0930edb2e3af621d02f07e78546adba66
ukkashah11 <115150510+ukkashah11@users.noreply.github.com>
2023-10-24 08:42:02 +0500

    Update Q2.c

--- Lab/08/Q2.c
+++ Lab/08/Q2.c
@@ -1,5 +1,14 @@
+/*
+Name: Syed Ukkashah
+Date: 10/24/23
+Decription: C program with a user-defined function calculate to perform basic arithmetic
+operations such as addition, subtraction, multiplication, and division. The
+program should take two numbers and an operation choice as input, and then use
+the function to perform the operation.
+
+*/
 #include <stdio.h>
-void calculate (double first, double second, char choice){
+void calculate (double first, double second, char choice){  //function takes 2 integers and a char choice as input, then uses switch cases to identify choice variable and perform appropriate artihmetic calculations on the two integers
 	 switch (choice) {
     case '+':
       printf("%lf + %lf = %lf", first, second, first + second);
