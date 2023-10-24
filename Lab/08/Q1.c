bcf0d0b29dacbd4bce70d6f16acf8a7f10624af3
ukkashah11 <115150510+ukkashah11@users.noreply.github.com>
2023-10-24 08:27:09 +0500

    Update Q1.c

--- Lab/08/Q1.c
+++ Lab/08/Q1.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-void swapInteger(int a, int b){
+/*a C program that swaps the values of two integers using a user-defined
+function, swapIntegers. The user inputs two integer values, and the program
+uses the function to swap them. It should perform the swap, and display the
+updated values.
+*/
+void swapIntegers(int a, int b){
 	int temp = a;
 	a=b;
 	b=temp;
@@ -15,6 +19,6 @@ int main() {
 	scanf("%d", &x);
 	printf("Enter a number b: ");
 	scanf("%d", &y);
-	swapInteger(x,y);
+	swapIntegers(x,y);
 	return 0;
 }
