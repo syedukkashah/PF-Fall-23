9222f2fea2ab6cd49c7935aa3b677e46278f0968
ukkashah11 <115150510+ukkashah11@users.noreply.github.com>
2023-11-01 23:06:30 +0500

    Update Q9.c

--- Assignments/2/Q9.c
+++ Assignments/2/Q9.c
@@ -40,22 +40,22 @@ int main(){
 	top=0, bottom = n-1, left=0, right = n-1;
 	cnt=n*n;
 	while(top<=bottom && left<=right){
-	for(int j=left; j<=right; j++){
+	for(int j=right; j>=left; j--){
 		arr[top][j] = cnt--;
 	}
 	top++;
 	for(int j=top; j<=bottom; j++){
-		arr[j][right] = cnt--;
+		arr[j][left] = cnt--;
 	}
-	right--;
-	for(int j=right; j>=left; j--){
+	left++;
+	for(int j=left; j<=right; j++){
 		arr[bottom][j]=cnt--;
 	}
 	bottom--;
 	for(int j=bottom; j>=top; j--){
-		arr[j][left]=cnt--;
+		arr[j][right]=cnt--;
 	}
-	left++;
+	right--;
 }
 	for(int i=0; i<n; i++){
 		for(int j=0; j<n; j++){
