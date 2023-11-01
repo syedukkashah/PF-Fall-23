fefe0b7c3824bee511668ba3c0f2cb13b84ae40e
ukkashah11 <115150510+ukkashah11@users.noreply.github.com>
2023-11-01 20:01:04 +0500

    Update Q5.c

--- Assignments/2/Q5.c
+++ Assignments/2/Q5.c
@@ -1,21 +1,24 @@
 #include<stdio.h>
 #include<math.h>	
-
+#include <stdlib.h>
 int main(){
 int n, temp=0;
 printf("Enter n to print all Ramanujan numbers less than n^3: ");
 scanf("%d", &n);
-while(temp<(pow(n,3))){
-for(int i=1; i++;){
-	for(int j=1; j++;){
-		for(int k=1; k++;){
-			for(int l=1; l++;){
+while(1){
+for(int i=1;i<n; i++){
+	for(int j=1;j<n;j++){
+		for(int k=1;k<n; k++){
+			for(int l=1;l<n; l++){
 				if(i!=j && i!=k && i!=l && j!=k && j!=l && k!=l){
 					int left = pow(i,3)+pow(j,3);
 					int right = pow(k,3)+pow(l,3);
 					if(right == left && left!=temp){
 					if(temp<left){
 						temp=left;
+						if(temp>pow(n,3)){
+							exit(0);
+						}
 						printf("%d = %d^3 + %d^3 = %d^3 + %d^3\n", left,i,j,k,l);
 					}
 				}
