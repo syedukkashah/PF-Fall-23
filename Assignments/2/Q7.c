8c111ea19d2a54e886f43506c8b1e5be6390d53e
ukkashah11 <115150510+ukkashah11@users.noreply.github.com>
2023-11-03 23:44:13 +0500

    Update Q7.c

--- Assignments/2/Q7.c
+++ Assignments/2/Q7.c
@@ -1,9 +1,12 @@
+//Programmer: Syed Ukkashah
+//Description: Using 2d arrays for sorting, etc.
+//Date: 28th October 2023
 #include <stdio.h>
-void sort(int arr [][2], int arr2[][2], int n){
+void sort(int arr [][2], int arr2[][2], int n){ // function takes both arrays and their size as arguments 
 	int temp;
 	for(int i=0; i<n; i++){
 		for(int j=0; j<n-1; j++){
-		if(arr[j][0]>arr[j+1][0]){
+		if(arr[j][0]>arr[j+1][0]){ // sorts arrays
 			temp = arr[j][0];
 			arr[j][0] = arr[j+1][0];
 			arr[j+1][0]=temp;
@@ -20,7 +23,7 @@ void sort(int arr [][2], int arr2[][2], int n){
 	}
 	}
 }
-void show(int arr [][2], int arr2[][2], int n){
+void show(int arr [][2], int arr2[][2], int n){ // function prints arrays in relevant order
 	printf("\nAscending order wrt to Age\n");
 	for(int i=0; i<n; i++){
 			printf("Shirt %d\n Age:%d --- Price: %d \n", i+1, arr[i][0], arr[i][1]);
@@ -36,7 +39,7 @@ void main(){
 	printf("Enter number of shirts: ");
 	scanf("%d", &n);
 	int shirt_ages[n][2], shirt_prices[n][2];
-	printf("Enter shirt details: \n");
+	printf("Enter shirt details: \n"); // input prompts
 	for(int i=0; i<n; i++){
 		printf("Shirt %d\n", i+1);
 			printf("Age: ");
@@ -46,6 +49,6 @@ void main(){
 			scanf("%d", &shirt_prices[i][0]);
 			shirt_ages[i][1] = shirt_prices[i][0];
 	}
-	sort(shirt_ages, shirt_prices, n);
+	sort(shirt_ages, shirt_prices, n); 
 	show(shirt_ages, shirt_prices, n);
 }
