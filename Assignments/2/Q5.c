5758cf23d4d3036bb0b3cf74cfb528db4f77b3f0
ukkashah11 <115150510+ukkashah11@users.noreply.github.com>
2023-11-03 23:41:49 +0500

    Update Q5.c

--- Assignments/2/Q5.c
+++ Assignments/2/Q5.c
@@ -1,8 +1,11 @@
+//Programmer: Syed Ukkashah
+//Description: Using nested loops to print Ramanujan numbers less than input n^3
+//Date: 28th October 2023
 #include<stdio.h>
-#include<math.h>	
+#include<math.h> //math.h library includes pow function	
 #include <stdlib.h>
 int main(){
-int n, temp=0;
+int n, temp=0; // initialization and declaration of variables
 printf("Enter n to print all Ramanujan numbers less than n^3: ");
 scanf("%d", &n);
 while(1){
@@ -10,14 +13,14 @@ for(int i=1;i<n; i++){
 	for(int j=1;j<n;j++){
 		for(int k=1;k<n; k++){
 			for(int l=1;l<n; l++){
-				if(i!=j && i!=k && i!=l && j!=k && j!=l && k!=l){
+				if(i!=j && i!=k && i!=l && j!=k && j!=l && k!=l){ //condition makes sure all pairs are unique
 					int left = pow(i,3)+pow(j,3);
 					int right = pow(k,3)+pow(l,3);
 					if(right == left && left!=temp){
-					if(temp<left){
+					if(temp<left){ //if number is repeated or less than printed number it doesn't print
 						temp=left;
 						if(temp>pow(n,3)){
-							exit(0);
+							exit(0); //program exits once condition is met
 						}
 						printf("%d = %d^3 + %d^3 = %d^3 + %d^3\n", left,i,j,k,l);
 					}
